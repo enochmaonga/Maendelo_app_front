ffa1361e6c644b7e831da1954d31e19a7a77f620
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-05-10 20:40:14 +0300

    Adding divs for Names, shop, accessories option

--- src/App.js
+++ src/App.js
@@ -17,32 +17,57 @@ class App extends Component{
                 <p>Nairobi</p>
                 </h5>
              <div>
+             <h6>Please fill in the details below</h6>
                <input  type="text"  name="name" placeholder="Retail Center Name"
                />
-             </div>
-             <div>
                <input  type="text"  name="name" placeholder="Retail Center Contact"
                />
              </div>
-             <h6>Please fill in the details below</h6>
              <div>
+               <input  type="text"  name="name" placeholder=" "
+               />
+             </div>
+             
+             <div>
+               <n6>Customer Name: </n6>
                <input  type="text"  name="name" placeholder="Name"
+               /> <n6>Customer Phone number: </n6> 
+                 <input type="number" name="phonenumber" placeholder="Customer Phone No."
                />
              </div>
              <div>
-               <input type="number" name="phonenumber" placeholder="Customer Phone Number"
-               />
+             
              </div>
              <div>
                <input type="text" name="name" placeholder="Item Make"
                />
-             </div>
-             <div>
-               <input type="text" name="name" placeholder="Item Model"
+                <input type="text" name="name" placeholder="Item Model"
+               />
+               <input type="number" name="name" placeholder="IMEI/Serial No"
+               />
+               <h5>ACCESSORIES ACCOMPANYING CELLPHONE: (SIM cards must not be sent with the device for repair)</h5>
+               <input type="checkbox" id="accessory" name="Battery" value="Battery"/>
+               <label for="accessory">Battery</label>
+               <input type="checkbox" id="accessory" name="Charger" value="Charger"/>
+               <label for="accessory">Charger</label>
+               <h5>Lipa Mdogomdogo</h5>
+               <input type="checkbox" id="Lipa" name="Lmm" value="Lmm"/> 
+               <label for="Lipa">Yes</label>
+               <input type="checkbox" id="Lipa" name="Lmm" value="Lmm"/> 
+               <label for="Lipa">No</label>
+
+              <h6>Is this repair a WARRANTY CLAIM?</h6>
+              <input type="text" name="name" placeholder="True/False"
+               />
+               <h6>Repair History</h6>
+               <input type="text" name="name" placeholder="True/False"
                />
              </div>
              <div>
-               <input type="number" name="name" placeholder="IMEI/Serial Number"
+              
+             </div>
+             <div>
+               <input type="number" name="name" placeholder=""
                />
              </div>
            </form>
