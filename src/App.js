ae534b4a6da7ad839fdb8ca97376628070765faa
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-05-10 19:34:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+
+
+class App extends Component{
+   render(){
+      return(
+         <div>
+           <form>
+             <div>
+             <h1>Safaricom Repair Booking Form</h1>
+             </div>
+             <h5>Safaricom House 
+                <p>Waiyaki Way </p>
+                <p>P. O. Box 66827 00800</p>
+                <p>Nairobi</p>
+                </h5>
+             <div>
+               <input  type="text"  name="name" placeholder="Retail Center Name"
+               />
+             </div>
+             <div>
+               <input  type="text"  name="name" placeholder="Retail Center Contact"
+               />
+             </div>
+             <h6>Please fill in the details below</h6>
+             <div>
+               <input  type="text"  name="name" placeholder="Name"
+               />
+             </div>
+             <div>
+               <input type="number" name="phonenumber" placeholder="Customer Phone Number"
+               />
+             </div>
+             <div>
+               <input type="text" name="name" placeholder="Item Make"
+               />
+             </div>
+             <div>
+               <input type="text" name="name" placeholder="Item Model"
+               />
+             </div>
+             <div>
+               <input type="number" name="name" placeholder="IMEI/Serial Number"
+               />
+             </div>
+           </form>
+         </div>
+      );
+   }
 }
 
 export default App;
---
+++ src/components/RepairForm.jsx
@@ -0,0 +1,18 @@
+export default function(){
+    return(
+        <div>
+            <form>
+                <div>
+                    <h3>Repair Form</h3>
+                </div>
+                <div>
+                    <input
+                    type="text"
+                    name= "name"
+                    placeholder= "name"
+                    />
+                </div>
+            </form>
+        </div>
+    )
+}
