6aebb1bf7dfd506bbceaf0270f55096593681f6e
Wangari Gathagu <rachealkari@gmail.com>
2022-08-19 02:26:29 +0300

    updating RetailCenterRequestsMenu view

--- src/components/RetailCenterRequestsMenu.js
+++ src/components/RetailCenterRequestsMenu.js
@@ -26,6 +26,7 @@ class RetailCenterRequestsMenu extends Component{
 
 
             <div >
+             <a href="/" class="btn btn-dark position-absolute top-1 end-0 m-3">Logout</a>
                 <div className="w-75 mx-auto pt-1">
                     <h6 className="display-6">Requests</h6>
                         <Navbar collapseOnSelect expand="lg">
